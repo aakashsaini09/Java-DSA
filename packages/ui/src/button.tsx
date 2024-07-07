1d789414783ce3bdd6f4d14be0fc9f37cfa9583a
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-07 15:13:05 +0530

    adding docker

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,9 +11,8 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+      className='h-30 w-40 bg-blue-500 text-white font-bold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75'
+      onClick={() => alert(`Hello from your ${appName} app!`)}>
       {children}
     </button>
   );
