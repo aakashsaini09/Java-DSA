221b390948991692f69ba58eb4e7497d60283a21
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-12 22:15:36 +0530

    update

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,10 +1,31 @@
 import express from 'express';
+import db from '@repo/db/client'
 const app = express();
-app.post("/hdfcwebhook", (req, res) => {
+app.post("/hdfcwebhook", async(req, res) => {
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.userId,
         amonund: req.body.amount, 
     };
-})
-
+    await db.balance.update({
+        where: {
+            userId: paymentInformation.userId
+        },
+        data: {
+            amount: {
+                increment: paymentInformation.amonund
+            }
+        }
+    })
+    await db.onRampTransaction.update({
+        where: {
+            token: paymentInformation.token
+        },
+        data: {
+            status: "Success"
+        }
+    })
+    res.json({
+        message: "captured"
+    })
+});
