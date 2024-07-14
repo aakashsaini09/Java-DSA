d2007034fc98c3c6ae8a5035f118aebe8425a92b
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-14 15:06:14 +0530

    updated

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -13,5 +13,3 @@ const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
-// npx prisma migrate dev
-// npx prisma generate
