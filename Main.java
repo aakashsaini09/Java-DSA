09d4c1b72a925a42e75a5e31d39e9351ff2e5c4d
aakashsaini09 <aakashsaini948585@gmail.com>
2025-09-10 22:24:19 +0530

    packages

---
+++ Main.java
@@ -0,0 +1,10 @@
+import algos.Arith;
+public class Main {
+    public static void main(String[] args) {
+        Arith obj = new Arith();
+        System.out.println("Sum: " + obj.SumOfTwo(10, 5));
+        System.out.println("Minus: " + obj.MinusOfTwo(10, 5));
+        System.out.println("Division: " + obj.DivOfTwo(10, 5));
+        System.out.println("Multiplication: " + obj.MinusOfTwo(10, 5));
+    }
+}
---
+++ algos/Arith.java
@@ -0,0 +1,16 @@
+package algos;
+
+public class Arith {
+    public int SumOfTwo(int a, int b){
+        return a + b;
+    }
+    public int MinusOfTwo(int a, int b){
+        return a - b;
+    }
+    public int DivOfTwo(int a, int b){
+        return a / b;
+    }
+    public int SubOfTwo(int a, int b){
+        return a * b;
+    }
+}
