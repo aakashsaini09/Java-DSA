040c9b7df91e8b3d1a6dbbe18a8de6121992f4cd
Aakash <aakashsaini948585@gmai.com>
2025-09-18 16:08:17 +0530

    leetcode qstn

--- Main.java
+++ Main.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 import algos.Arith;
+import algos.Math;
 public class Main {
     public static void main(String[] args) {
-        Arith obj = new Arith();
-        System.out.println("Sum: " + obj.SumOfTwo(10, 5));
-        System.out.println("Minus: " + obj.MinusOfTwo(10, 5));
-        System.out.println("Division: " + obj.DivOfTwo(10, 5));
-        System.out.println("Multiplication: " + obj.MinusOfTwo(10, 5));
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter 1 for Arithmetic operations and 2 for other math operations.");
+            int selection = sc.nextInt();
+            if(selection == 1){
+                System.out.println("Enter two numbers: ");
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                Arith arithmetic = new Arith();
+                System.out.println("Sum is: " + arithmetic.SumOfTwo(a, b));
+                System.out.println("Minus is: " + arithmetic.MinusOfTwo(a, b));
+                System.out.println("Division is: " + arithmetic.DivOfTwo(a, b));
+                System.out.println("Multiplication is: " + arithmetic.MinusOfTwo(a, b));
+            }else if(selection == 2){
+                System.out.print("Enter One numbers: ");
+                int a = sc.nextInt();
+                Math mathFunctions = new Math();
+                System.out.println("Square is: " + mathFunctions.SquareNumber(a));
+                System.out.println("Multiplication is: " + mathFunctions.CubeOfNumber(a));
+            }else{
+                System.out.println("Exiting the loop");
+                break;
+            }
+        }
     }
 }
---
+++ algos/Math.java
@@ -0,0 +1,10 @@
+package algos;
+
+public class Math {
+    public int SquareNumber(int a){
+        return a * a;
+    }
+    public int CubeOfNumber(int a){
+        return a * a * a;
+    }
+}
