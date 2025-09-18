768861d4dfa1eba06f3bd57c9b5e07f72d8e2ce5
Aakash <aakashsaini948585@gmai.com>
2025-09-18 19:21:37 +0530

    bubble sort

---
+++ Qstn23.java
@@ -0,0 +1,16 @@
+public class Qstn23 {
+    public static void main(String[] args) {
+        
+    }
+    // public int[] searchRange(int[] nums, int target) {
+    //     for(int i = 0; i <= nums.length; i++){
+    //         if (nums[i] == target) {
+    //             return i;
+    //             break;
+    //         }
+    //     }
+    //     for(int j = 0; j <= nums.length; j++){
+
+    //     }
+    // }
+}
